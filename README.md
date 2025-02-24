e4dda10b58242093a0f64dbe308636ae7d9411cb
mtyler <michael.tyler@gmail.com>
2025-02-24 11:49:02 -0500

    update the containerd config placement

--- README.md
+++ README.md
@@ -1,61 +1,42 @@
 # Klima
 
-Is a slightly opinionated wrapper script around Lima VM. The purpose is to create a minimally viable, just-enough-k8s cluster to operate kubernetes in a local (re: laptop) environment. Kubernetes is deployed with kubeadm on ubuntu Lima VMs. 
+Klima is a slightly opinionated wrapper script around Lima VM. The purpose is to create a minimally viable, just-enough-k8s cluster to operate Kubernetes in a local (re: laptop) environment. Kubernetes is deployed with kubeadm on Ubuntu Lima VMs.
 
-Klima provisions a Virtual Machine based kubernetes cluster and facilitates the attachment of raw disks. 
+Klima provisions a Virtual Machine-based Kubernetes cluster and facilitates the attachment of raw disks. It is meant to stand up and tear down quickly and easily.
 
-Meant to standup and teardown quickly and easily.
+## Dependencies and Pre-requisites
 
-## Dependancies and Pre-reqs
-
-Lima VM (brew install)
-socket_vmnet (binary install)
-qemu (brew install)
-Python3 (brew install)
+- **Lima VM** (brew install)
+- **socket_vmnet** (binary install)
+- **qemu** (brew install)
+- **Python3** (brew install)
 
 ## Usage
 
-Beware... this is still a bit WIPy
+- `klima.py --up` starts a 4-node cluster
+- `klima.py --klober` deletes the cluster
 
-klima.py --up starts a 4 node cluster
+### Install and Verify socket_vmnet & QEMU
 
-klima.py --klober deletes the cluster
-
-
-### verify socket_vmnet & QEMU
-
+```sh
 brew install qemu
 
 /opt/socket_vmnet/bin/socket_vmnet_client /var/run/socket_vmnet qemu-system-aarch64 \
 -device virtio-net-pci,netdev=net0 -netdev socket,id=net0,fd=3 -m 4096 -accel hvf -cdrom /Users/mtyler/Downloads/ubuntu-24.04.1-live-server-arm64.iso
 
+mkdir -p /var/run
+sudo /opt/socket_vmnet/bin/socket_vmnet --vmnet-gateway=192.168.105.1 /var/run/socket_vmnet
+```
 
-mkdir -p /var/run sudo /opt/socket_vmnet/bin/socket_vmnet --vmnet-gateway=192.168.105.1 /var/run/socket_vmnet
+## Storage
 
-
-## storage
-lima provides ext4 formatted partition
-
-ceph / rook - required unpartitioned drive
-nfs is possible but does not provide the type of service falco requires
-topolvm - requires a Volume Group. VG requires a non-partitioned disk
-
-
+- **lima** provides ext4 formatted partition
+- **ceph / rook** - requires unpartitioned drive
+- **nfs** is possible but does not provide the type of service falco requires
+- **topolvm** - requires a Volume Group. VG requires a non-partitioned disk
 
 ### Issues
 
-#### time is not staying up to date
-installed and configured chrony
-issue is slightly better but still persists
+- Time is Not Staying Up to Date: Chrony has been installed but something is causeing the clock to jump forward. When this happens, the only resolution I'vefound is to restart the node.
 
-[ 1128.763113] rcu: INFO: rcu_preempt self-detected stall on CPU
-[ 1128.763203] rcu: 	0-...!: (6 ticks this GP) idle=4a6c/1/0x4000000000000002 softirq=71946/71948 fqs=0
-[ 1128.763208] rcu: 	(t=3002413905695 jiffies g=82521 q=3405 ncpus=2)
-[ 1128.763213] rcu: rcu_preempt kthread timer wakeup didn't happen for 3002413905692 jiffies! g82521 f0x0 RCU_GP_WAIT_FQS(5) ->state=0x402
-[ 1128.763216] rcu: 	Possible timer handling issue on cpu=1 timer-softirq=18806
-[ 1128.763218] rcu: rcu_preempt kthread starved for 3002413905695 jiffies! g82521 f0x0 RCU_GP_WAIT_FQS(5) ->state=0x402 ->cpu=1
-[ 1128.763221] rcu: 	Unless rcu_preempt kthread gets sufficient CPU time, OOM is now expected behavior.
-[ 1128.763223] rcu: RCU grace-period kthread stack dump:
-[ 1128.763225] task:rcu_preempt     state:I stack:0     pid:17    tgid:17    ppid:2      flags:0x00000008
 
-increased cpu to 4, mem left at 8GB
--- klima.py
+++ klima.py
@@ -7,7 +7,6 @@ import sys
 import signal
 from time import sleep, time
 
-template = "k8s-base.yaml"
 CWD = f"{os.path.dirname(os.path.abspath(__file__))}/"
 klima_work_dir = ".klima/k8scfg"
 first_cp_name = "cp1"
@@ -16,7 +15,7 @@ USER_HOME_DIR = os.path.expanduser("~")
 
 class Knode:
     k8s_prefix = "lima-"
-    template = "k8s-base.yaml"
+    template = "k8s-cluster.yaml"
     
     def __init__(self, name, cpu, mem, disk_size, additional_disk_size, role):
         self.name = name
@@ -27,33 +26,50 @@ class Knode:
         self.role = role
 
     def create(self):
-        #print(f"Creating {self.name}")
+        if debug: print(f"Creating {self.name}")
         with_disk = ""
         if self.additional_disk_size:
-            run_command(f"limactl disk create {self.get_diskname()} --size={self.additional_disk_size} --format=raw")
+            run_cmd(f"limactl disk create {self.get_diskname()} --size={self.additional_disk_size} --format=raw")
             with_disk = f"| .additionalDisks[0].name = \"{self.get_diskname()}\" | .additionalDisks[0].format = false"
-        
-        return run_command(f"limactl create --name={self.name} {self.template} --set '.disk = \"{self.disk_size}\" | .cpus = {self.cpu} | .memory = \"{self.mem}\" {with_disk}' --tty=false")
 
-    def get_diskname(self):
-        return f"{self.name}-data"
-    
+        # automating the command limactl create --name=cp1 k8s-cluster.yaml --set '.disk = "30GiB" | .cpus = 2 | .memory = "4GiB"' --tty=false 
+        return run_cmd(f"limactl create --name={self.name} {self.template} --set '.disk = \"{self.disk_size}\" | .cpus = {self.cpu} | .memory = \"{self.mem}\" {with_disk}' --tty=false")
+
     def start(self):
-        #print(f"Starting {self}")
-        run_command(f"limactl start {self.name} --tty=false")
+        if debug: print(f"Starting {self}")
+        #run_command(f"limactl start {self.name} --tty=false")
+        run_cmd(f"limactl start --name={self.name} --tty=false")
 
     def stop(self):
-        run_command(f"limactl stop {self.name}")
+        run_cmd(f"limactl stop {self.name}")
+
+        
+    def kill_vm(self):
+        if debug: print(f"Stopping and removing {self.name}")
+        if self.is_vm():
+            subprocess.run(['limactl', 'stop', self.name])
+            subprocess.run(['limactl', 'delete', self.name])
+        else:
+            if debug: print(f"{self.name} is not a VM")
+
+    def pull_node(self):
+        if debug: print(f"Draining and removing node {self.name}")
+        subprocess.run(['kubectl', 'drain', f"lima-{self.name}", '--delete-local-data', '--force', '--ignore-daemonsets'])
+        subprocess.run(['kubectl', 'delete', 'node', f"lima-{self.name}"])
 
     def is_vm(self):
-        #print(f"Checking if {self.name} is a VM")
+        if debug: print(f"Checking if {self.name} is a VM")
         return self.name in self.get_vm_names()
     
+    def get_vm_names(self):
+        result = subprocess.run(['limactl', 'list', '--format', '{{.Name}}'], capture_output=True, text=True)
+        return result.stdout.splitlines()
+
     def is_ready(self):
         if not self.is_vm():
             return False
         node = f"{self.k8s_prefix}{self.name}"
-        #print(f"Checking if {node} is ready")
+        if debug: print(f"Checking if {node} is ready")
         #result = subprocess.run(['kubectl', 'get', 'node', node, '-o=jsonpath={.metadata.name}'], capture_output=True, text=True)
         cmd = ['kubectl', 'get', 'node', node, '-o=jsonpath={.metadata.name}']
         result = run_cmd(cmd)
@@ -65,19 +81,11 @@ class Knode:
     def is_leader(self):
         return self.role == "leader"
     
-    def kill_vm(self):
-        print(f"Stopping and removing {self.name}")
-        subprocess.run(['limactl', 'stop', self.name])
-        subprocess.run(['limactl', 'delete', self.name])
+    def get_diskname(self):
+        return f"{self.name}-data"
 
-    def pull_node(self):
-        print(f"Draining and removing node {self.name}")
-        subprocess.run(['kubectl', 'drain', f"lima-{self.name}", '--delete-local-data', '--force', '--ignore-daemonsets'])
-        subprocess.run(['kubectl', 'delete', 'node', f"lima-{self.name}"])
-
-    def get_vm_names(self):
-        result = subprocess.run(['limactl', 'list', '--format', '{{.Name}}'], capture_output=True, text=True)
-        return result.stdout.splitlines()
+    def remove_disk(self):
+        subprocess.run(['limactl', 'disk', 'delete', self.get_diskname()])
 
     def get_disks(self):
         result = subprocess.run(['limactl', 'disk', 'list'], capture_output=True, text=True)
@@ -89,9 +97,6 @@ class Knode:
             if len(parts) > 0 and parts[0] != 'NAME':
                 disk_names.append(parts[0])
         return disk_names
-
-    def remove_disk(self):
-        subprocess.run(['limactl', 'disk', 'delete', self.get_diskname()])
 
 class Kluster:
     # Node Role:
@@ -108,21 +113,23 @@ class Kluster:
 
     FOUR_NODE_TOPOLOGY = {
         #"n3":  {"cpu": 2, "mem": 4, "disk_size": "30GiB", "raw_disk_size": "50GiB", "role": "worker"}
-        Knode("cp1", 4, "8GiB", "30GiB", "30GiB", "leader"),
-        Knode("n1",  4, "8GiB", "30GiB", "50GiB", "worker"),
-        Knode("n2",  4, "8GiB", "30GiB", "50GiB", "worker"),
-        Knode("n3",  4, "8GiB", "30GiB", "50GiB", "worker")
+        Knode("cp1", 4, "4GiB", "30GiB", "30GiB", "leader"),
+        Knode("n1",  4, "4GiB", "30GiB", "50GiB", "worker"),
+        Knode("n2",  4, "4GiB", "30GiB", "50GiB", "worker"),
+        Knode("n3",  4, "4GiB", "30GiB", "50GiB", "worker")
     }
     work_dir = f"{CWD}.klima/k8scfg"
     config_dir = f"{USER_HOME_DIR}/.kube"
     template = "k8s-base.yaml"
 
-    def __init__(self, topology=None):
+    def __init__(self, topology=None, name="FOUR_NODE_TOPOLOGY"):
         if topology is None:
             self.topology = self.FOUR_NODE_TOPOLOGY
-            self.name = "FOUR_NODE_TOPOLOGY"
+            self.name = name
         else:
             self.topology = topology
+            self.name = name
+        os.makedirs(self.work_dir, exist_ok=True)
 
     def is_up(self):
         return self.get_leader().is_ready()
@@ -134,6 +141,10 @@ class Kluster:
         result = subprocess.run(['limactl', 'list', '--format', '{{.Name}}'], capture_output=True, text=True)
         return result.stdout.splitlines()
     
+    def get_disk_names(self):
+        result = subprocess.run(['limactl', 'disk', 'list'], capture_output=True, text=True)
+        lines = result.stdout.splitlines()
+
     def get_leader(self):
         for node in self.topology:
             if node.role == "leader":
@@ -152,21 +163,21 @@ class Kluster:
             if node.role == "worker":
                 workers.append(node)
         
-        print(f"# workers is {len(workers)}")
+        if debug: print(f"# workers is {len(workers)}")
         return workers
     
     def get_kubeconfig(self):
         # Copy the kubeconfig file to the host
-        print("Updating ~/.kube/config...")
+        if debug: print("Updating ~/.kube/config...")
         subprocess.run(['cp', f"{self.work_dir}/admin.conf", f"{self.config_dir}/config"])
 
-    def create(self):
-        print(f"Creating cluster {self.name}")
-        os.makedirs(self.work_dir, exist_ok=True)
+#    def create(self):
+#        print(f"Creating cluster {self.name}")
+#        os.makedirs(self.work_dir, exist_ok=True)
 
     def destroy(self):
-        print(f"Destroying cluster {self.name}")
-        run_command(f"rm -rf {self.work_dir}")
+        if debug: print(f"Destroying cluster {self.name}")
+        run_cmd(f"rm -rf {self.work_dir}")
 
 def run_cmd(command):
     try: 
@@ -184,84 +195,16 @@ def run_cmd(command):
     finally:
         pass
 
-
-def run_command(command):
-    if isinstance(command, list):
-        result = subprocess.run(command, capture_output=True, text=True)
-    else:
-        result = subprocess.run(command, shell=True)
-    if result.stdout:
-        print(result.stdout.decode('utf-8'))
-    if result.stderr:
-        print(result.stderr.decode('utf-8'))
-    return
-
-##def stop_and_remove_vm(vm_name):
-##    print(f"Stopping and removing {vm_name}")
-##    subprocess.run(['limactl', 'stop', vm_name])
-##    subprocess.run(['limactl', 'delete', vm_name])
-##
-##def remove_disk(d_name):
-##    subprocess.run(['limactl', 'disk', 'delete', d_name])
-##
-##def drain_and_remove_node(vm_name):
-##    print(f"Draining and removing node {vm_name}")
-##    subprocess.run(['kubectl', 'drain', f"lima-{vm_name}", '--delete-local-data', '--force', '--ignore-daemonsets'])
-##    subprocess.run(['kubectl', 'delete', 'node', f"lima-{vm_name}"])
-
-##def klober_main(args):
-##    if args.node:
-##        vm_names = get_vm_names()
-##        if args.node in vm_names:
-##            drain_and_remove_node(args.node)
-##            stop_and_remove_vm(args.node)
-##            print(f"VM {args.node} stopped and removed")
-##        return
-##    
-##    if not args.force:
-##        input("!!! WARNING !!! \n\n \
-##This is a permanent action. All lima VMs and disks will be lost forever. \n \
-##Press Enter to continue...\n")
-##    
-##    vm_names = get_vm_names()
-##    for vm_name in vm_names:
-##        stop_and_remove_vm(vm_name)
-##    print("VMs stopped and removed")
-##
-##    d_names = get_disk_names()
-##    for d_name in d_names:
-##        remove_disk(d_name)
-##    print("Disks removed")
-##    
-##    subprocess.run(['rm', '-rf', './.klima'])
-##    print("klima working directory removed")
-##
-##    subprocess.run(['limactl', 'list'])
-##    subprocess.run(['limactl', 'disk', 'list'])
-
-
-##def get_disk_names():
-##    result = subprocess.run(['limactl', 'disk', 'list'], capture_output=True, text=True)
-##    lines = result.stdout.splitlines()
-##    disk_names = []
-##    for line in lines:
-##        parts = line.split()
-##        if len(parts) > 0 and parts[0] != 'NAME':
-##            disk_names.append(parts[0])
-##    return disk_names
-##
-##def get_vm_names():
-##    result = subprocess.run(['limactl', 'list', '--format', '{{.Name}}'], capture_output=True, text=True)
-##    return result.stdout.splitlines()
-##
-##def verify_node(node):
-##    result = subprocess.run(['kubectl', 'get', 'node', node, '-o=jsonpath={.metadata.name}'], capture_output=True, text=True)
-##    if result.stdout.splitlines()[0] == node:
-##        print(f"{node} is ready")
-##        return True
-##    else:
-##        print(f"{node} is not ready")
-##        return False
+#def run_command(command):
+#    if isinstance(command, list):
+#        result = subprocess.run(command, capture_output=True, text=True)
+#    else:
+#        result = subprocess.run(command, shell=True)
+#    if result.stdout:
+#        print(result.stdout.decode('utf-8'))
+#    if result.stderr:
+#        print(result.stderr.decode('utf-8'))
+#    return
 
 ## Bring up an individual node
 def node_up(node):
@@ -287,21 +230,21 @@ def node_up(node):
 
 ## Tear down an individual node
 def node_down(node):
-    print(f"Bringing down node {node.name}")
+    # print(f"Bringing down node {node.name}")
     node.kill_vm()
     node.remove_disk()
 
 ## Bring up a cluster topology
 def cluster_up(cluster):
     if not cluster.is_up():
-        cluster.create()
+        #cluster.create()
         # 1st Task: Start the initial control plane
         leader = cluster.get_leader()
         node_up(leader)
         cluster.get_kubeconfig()
     
     for node in cluster.get_workers():
-        print(f"Bring up worker {node.name}")
+        if debug: print(f"Bring up worker {node.name}")
         node_up(node)
 
 ## Tear down a cluster topology
@@ -314,24 +257,27 @@ Press Enter to continue...\n")
     for node in cluster.get_nodes():
         node_down(node)
     
+    print(f"VMs remaining: {cluster.get_vm_names()}")
+    print(f"Disks remaining: {cluster.get_disk_names()}")
+    
 
 def main(args):
     cluster = Kluster()
     if args.up:
         cluster_up(cluster)
     elif args.klober:
-#        klober_main(args)
         cluster_down(cluster, args.force)
         cluster.destroy()
  
-def signal_handler(sig, frame): 
-    sys.exit(0)
-        
 if __name__ == "__main__":
     start_time = time()
     def print_total_time():
         total_time = time() - start_time
         print(f"Execution time: {total_time / 60:.2f} minutes")
+
+    def signal_handler(sig, frame):
+        print_total_time()
+        sys.exit(0)
 
     parser = argparse.ArgumentParser(description="Manage lima VMs and disks")
     task_group = parser.add_mutually_exclusive_group(required=True)
@@ -342,9 +288,13 @@ if __name__ == "__main__":
     parser.add_argument('--cp1disk', required=False, action='store_true', help='add data disk to cp1')
     parser.add_argument('--node', '-n', required=False, type=str, help='remove a single node from a k8s cluster')
     parser.add_argument('--force', '-f', required=False, action='store_true', help='force remove all VMs and disks')
+    parser.add_argument('--debug', '-d', required=False, action='store_true', help='debug mode')
     args = parser.parse_args()
     signal.signal(signal.SIGINT, signal_handler)
     
+    global debug
+    debug = args.debug
+
     main(args)
 
     print_total_time()
